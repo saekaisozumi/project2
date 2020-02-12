f088f0d30a49ca9243bf585732042bd8b7cc0a80
Saeka Isozumi <isozumisaeka@gmail.com>
2020-02-12 16:21:42 +0100

    map done and css

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const yelp = require("yelp-fusion");
+const apiKey = process.env.apiKeyYelp;
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   console.log(req.user);
   res.render("index", { user: req.user });
+});
+
+router.get("/caffees", (req, res, next) => {
+  const searchRequest = {
+    location: "mitte,berlin",
+    // categories: “cafes”,
+    categories: "internetcafe"
+  };
+  const client = yelp.client(apiKey);
+  client
+    .search(searchRequest)
+    .then(response => {
+      let venues = response.jsonBody.businesses;
+      res.json(venues);
+    })
+    .catch(e => {
+      console.log(e);
+    });
 });
 
 /* router.get("/main", (req, res, next) => {
